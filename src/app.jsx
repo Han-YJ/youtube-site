c99f4d28ca13a2f4f6fba78ccd3fd14c4dfdb114
Han <lucirhk@gmail.com>
2021-02-24 17:03:21 +0900

    Component: VideoList

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,31 @@
 import './app.css';
 import Navbar from './components/navbar';
+import VideoList from './components/videoList';
 
-function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    videoList: [
+      {id: 1, thumbnail: '../images/logo.png',title: 'twice', userName: 'jyp'}, 
+      {id: 2, thumbnail: '../images/logo1.png',title: 'twice1', userName: 'jyp'},
+      {id: 3, thumbnail: '../images/logo2.png',title: 'twice2', userName: 'jyp'}
+    ],
+  };
+
+  render() {
+    console.log(this.state.requestOptions);
+    return (
+      <>
+        <Navbar></Navbar>
+        <VideoList
+          videoList = {this.state.videoList}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
